f7ebc0e52e3f2aaf774e192f575622e33138669a
Kristoffer Johansen <kj@computas.com>
2018-12-21 11:57:43 +0100

    my first speedtestlogger

---
+++ SpeedTestLogger/LoggerConfiguration.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Globalization;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace SpeedTestLogger
+{
+    // Omitting namespace
+    public class LoggerConfiguration
+    {
+        public readonly string UserId;
+        public readonly int LoggerId;
+        public readonly RegionInfo LoggerLocation;
+
+        public LoggerConfiguration()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            var configuration = builder.Build();
+
+            // Code continues here
+            var countryCode = configuration["loggerLocationCountryCode"];
+            LoggerLocation = new RegionInfo(countryCode);
+
+            Console.WriteLine("Logger located in {0}", LoggerLocation.EnglishName);
+            UserId = configuration["userId"];
+            LoggerId = Int32.Parse(configuration["loggerId"]);
+        }
+    }
+}
